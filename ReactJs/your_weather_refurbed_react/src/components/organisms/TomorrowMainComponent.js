657944cd87c380d9a308e570bf07a92b02eba41c
Danny <114002047+DannyFink1@users.noreply.github.com>
2024-03-03 12:12:24 +0100

    fixed min/max temp calculation

--- ReactJs/your_weather_refurbed_react/src/components/organisms/TomorrowMainComponent.js
+++ ReactJs/your_weather_refurbed_react/src/components/organisms/TomorrowMainComponent.js
@@ -8,12 +8,29 @@ export default function TomorrowMainComponent({ timeIndex, timeTitle }) {
     let time = tomorrowRange.forecast.forecastday[0].hour[timeIndex];
     console.log(time);
 
+    function minMaxTempInRange() {
+
+        var startIndex = timeIndex - 3;
+        var endIndex = timeIndex + 3;
+
+        var minTemp = Infinity;
+        var maxTemp = -Infinity;
+
+        for (var i = startIndex; i <= endIndex; i++) {
+            var temp = tomorrowRange.forecast.forecastday[0].hour[i].temp_c;
+            minTemp = Math.min(minTemp, temp);
+            maxTemp = Math.max(maxTemp, temp);
+        }
+
+        return { minTemp: minTemp, maxTemp: maxTemp };
+    }
+
     return (
         <div class="bg-white h-auto w-[90vw] md:w-[70vw] rounded-[20px] m-2 border-solid border-black border-[2px] flex justify-evenly relative p-3">
             <div class="flex items-center">
                 <div class="flex flex-row">
                     <div class="text-[40px] md:text-[50px] font-bold"> {time.temp_c}°</div>
-                    <div class="text-[15px] font-bold hidden md:inline self-start"> {tomorrowRange.forecast.forecastday[0].hour[timeIndex - 3].temp_c}°/ {tomorrowRange.forecast.forecastday[0].hour[timeIndex + 3].temp_c}°</div>
+                    <div class="text-[15px] font-bold hidden md:inline self-start"> {minMaxTempInRange().minTemp}°/ {minMaxTempInRange().maxTemp}°</div>
                 </div>
             </div>
             <div class="flex flex-col justify-center">
